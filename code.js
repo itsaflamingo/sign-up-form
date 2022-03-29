b1a17b2f38e01d54e77293d3386979528f503389
Elizabeth <elizabeth.pinero.r@gmail.com>
2022-03-29 11:55:31 -0400

    Changed visuals (background, color) Made more responsive by attaching side-legend background picture to the div via css background. Made top text more responsive. Cleaned up CSS and HTML to be more comprehensive nad readable.

--- code.js
+++ code.js
@@ -1,8 +1,28 @@
+const fName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+
+const phoneNumber = document.getElementById('phoneNumber');
+const email = document.getElementById('eMail');
 const pass = document.getElementById('password');
 const passConfirm = document.getElementById('confirmPassword');
 
-function alert() {
-    if (pass !== passConfirm) {
-        return "Password doesn't match";
+
+const errorDiv = document.getElementById('error');
+const form = document.getElementById('form');
+
+const passErrorDiv = document.getElementById('passwordError');
+const cPassErrorDiv = document.getElementById('cPasswordError');
+
+form.addEventListener('submit', (e) => {
+    let errors = [];
+
+    if(pass.value !== passConfirm.value) {
+        errors.push("Passwords don't match");
+        passErrorDiv.textContent = "Passwords don't match";
+        cPassErrorDiv.textContent = "Passwords don't match";
     }
-}
+    
+    if (errors.length > 0) {
+        e.preventDefault();
+    }
+})
